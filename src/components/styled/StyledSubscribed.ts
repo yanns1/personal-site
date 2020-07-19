fee5e053061aaf0de210f6ffe6598c3b43dac73c
Yann Salmon <coolcode.ys@gmail.com>
2020-07-19 10:33:40 +0200

    Implement Subscribed page

---
+++ src/components/styled/StyledSubscribed.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledSubscribed = styled.div`
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+`
+
+export default StyledSubscribed;
--- src/pages/subscribed.tsx
+++ src/pages/subscribed.tsx
@@ -1,13 +1,20 @@
 import React from "react"
-import { PageProps } from "gatsby";
+import { PageProps } from "gatsby"
+// components
+import Layout from "../components/shared/layout"
+import SEO from "../components/shared/seo"
+import StyledSubscribed from "../components/styled/StyledSubscribed";
 
-const Subscribed: React.FC<PageProps> = () => {
-
+const Portfolio: React.FC<PageProps> = ({ location }) => {
   return (
-    <div>
-      Subscribed
-    </div>
+    <Layout location={location}>
+      <SEO title="Subscribed" description="Confirm subscription to the newsletter" />
+      <StyledSubscribed><h1>Confirmed subscription !</h1>
+        <p>Again, thank you for joining the newsletter.</p>
+        <p>You can expect to receive emails when a new post is published.</p>
+        <p>If I want to send you other things in the future, I will contact you to request approval first.</p></StyledSubscribed>
+    </Layout>
   )
 }
 
-export default Subscribed
+export default Portfolio
